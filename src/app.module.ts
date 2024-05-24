b34eec3c39b92a6af8ae87035897da590a3febc3
JCases <javiercasessempere@gmail.com>
2024-05-24 10:19:53 +0200

    Add Services and CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { MessageModule } from './message/message.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, MessageModule, NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/message/message.service.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { MessageService } from './message.service';
+
+import { PrismaService } from './../prisma.service';
+
+describe('MessageService', () => {
+  let service: MessageService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [MessageService, PrismaService],
+    }).compile();
+
+    service = module.get<MessageService>(MessageService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
